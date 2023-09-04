b5cf2fa10233bbf36b5500cacec3541446653c42
Shuvonraj <125419101+Shuvonraj@users.noreply.github.com>
2023-09-04 14:01:46 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Banner from "./Components/Banner"
+import Navbar from "./Components/Navbar"
+import Service from "./Components/Service"
+
+
+function App() {
+ 
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <Banner></Banner>
+      <Service></Service>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Service.jsx
@@ -0,0 +1,107 @@
+import React from 'react'
+
+const Service = () => {
+    return (
+        <>
+            <section className='bg-[#FFF7E7] pt-[168px]'>
+                <div className='max-w-container mx-auto'>
+                    <div className='mx-auto text-center'>
+                        <h1 className='font-Oxanium text-primary font-bold text-[32px] '>SERVICES</h1>
+                        <h2 className='font-Raleway text-[39px] font-extrabold mb-[86px]'>Our services for you</h2>
+                    </div>
+                    <div className='flex gap-x-6'>
+                        <div className='pt-[67px] bg-white pb-12 px-[61px] font-Raleway  rounded-2xl drop-shadow	'>
+                            <svg width="71" className='mx-auto' height="86" viewBox="0 0 71 86" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.6" d="M0.492676 33.5C0.492676 14.9985 15.4911 0 33.9927 0H36.9631C55.4647 0 70.4631 14.9985 70.4631 33.5C70.4631 52.0015 55.4647 67 36.9631 67H33.9927C15.4911 67 0.492676 52.0015 0.492676 33.5Z" fill="#FFAF0F" />
+                                <path d="M57.5 47.8889H46.5V42.4444L41 37H30L24.5 42.4444V47.8889H13.5C10.475 47.8889 8 50.3389 8 53.3333V66.9444C8 68.9861 9.1 70.7011 10.75 71.6539V80.5556C10.75 83.5772 13.1975 86 16.25 86H54.75C57.8025 86 60.25 83.5772 60.25 80.5556V71.6267C61.8725 70.6739 63 68.9317 63 66.9444V53.3333C63 50.3389 60.525 47.8889 57.5 47.8889ZM30 42.4444H41V47.8889H30V42.4444ZM13.5 53.3333H57.5V66.9444H43.75V58.7778H27.25V66.9444H13.5V53.3333ZM38.25 69.6667H32.75V64.2222H38.25V69.6667ZM54.75 80.5556H16.25V72.3889H27.25V75.1111H43.75V72.3889H54.75V80.5556Z" fill="#F95C19" />
+                            </svg>
+                            <h1 className='font-Raleway font-extrabold text-2xl mt-7 mb-8 text-center mx-auto'>Business Services</h1>
+                            <p className='w-[302px] leading-6 ml-auto font-Raleway mb-[35px] '>We give you complete reliable delivery for your company.  We will take full responsibility of the deliveries.</p>
+                            <div className='text-center'>
+                                <div className='flex items-center font-normal '>
+                                    <svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <ellipse cx="4.2709" cy="3.5" rx="3.65517" ry="3.5" fill="#F95C19" />
+                                    </svg>
+                                    <h1 className='ml-[11.5px]'>Corporate goods</h1>
+                                </div>
+                                <div className='flex items-center  my-[11px]'>
+                                    <svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <ellipse cx="4.2709" cy="3.5" rx="3.65517" ry="3.5" fill="#F95C19" />
+                                    </svg>
+                                    <h1 className='ml-[11.5px]'>Shipment</h1>
+                                </div>
+                                <div className='flex items-center'>
+                                    <svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <ellipse cx="4.2709" cy="3.5" rx="3.65517" ry="3.5" fill="#F95C19" />
+                                    </svg>
+                                    <h1 className='ml-[11.5px]'>Accesories</h1>
+                                </div>
+                                <button className='px-24 py-3 border-4 hover:bg-primary hover:text-white border-primary font-Oxanium text-primary mt-[60px] '>Learn more </button>
+                            </div>
+                        </div>
+                        <div className='pt-[67px] bg-white pb-12 px-[61px] font-Raleway  rounded-2xl drop-shadow	'>
+                            <svg width="71" className='mx-auto' height="86" viewBox="0 0 71 86" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.6" d="M0.492676 33.5C0.492676 14.9985 15.4911 0 33.9927 0H36.9631C55.4647 0 70.4631 14.9985 70.4631 33.5C70.4631 52.0015 55.4647 67 36.9631 67H33.9927C15.4911 67 0.492676 52.0015 0.492676 33.5Z" fill="#FFAF0F" />
+                                <path d="M57.5 47.8889H46.5V42.4444L41 37H30L24.5 42.4444V47.8889H13.5C10.475 47.8889 8 50.3389 8 53.3333V66.9444C8 68.9861 9.1 70.7011 10.75 71.6539V80.5556C10.75 83.5772 13.1975 86 16.25 86H54.75C57.8025 86 60.25 83.5772 60.25 80.5556V71.6267C61.8725 70.6739 63 68.9317 63 66.9444V53.3333C63 50.3389 60.525 47.8889 57.5 47.8889ZM30 42.4444H41V47.8889H30V42.4444ZM13.5 53.3333H57.5V66.9444H43.75V58.7778H27.25V66.9444H13.5V53.3333ZM38.25 69.6667H32.75V64.2222H38.25V69.6667ZM54.75 80.5556H16.25V72.3889H27.25V75.1111H43.75V72.3889H54.75V80.5556Z" fill="#F95C19" />
+                            </svg>
+                            <h1 className='font-Raleway font-extrabold text-2xl mt-7 mb-8 text-center mx-auto'>Business Services</h1>
+                            <p className='w-[302px] leading-6 ml-auto font-Raleway mb-[35px] '>We give you complete reliable delivery for your company.  We will take full responsibility of the deliveries.</p>
+                            <div className='text-center'>
+                                <div className='flex items-center font-normal '>
+                                    <svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <ellipse cx="4.2709" cy="3.5" rx="3.65517" ry="3.5" fill="#F95C19" />
+                                    </svg>
+                                    <h1 className='ml-[11.5px]'>Corporate goods</h1>
+                                </div>
+                                <div className='flex items-center  my-[11px]'>
+                                    <svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <ellipse cx="4.2709" cy="3.5" rx="3.65517" ry="3.5" fill="#F95C19" />
+                                    </svg>
+                                    <h1 className='ml-[11.5px]'>Shipment</h1>
+                                </div>
+                                <div className='flex items-center'>
+                                    <svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <ellipse cx="4.2709" cy="3.5" rx="3.65517" ry="3.5" fill="#F95C19" />
+                                    </svg>
+                                    <h1 className='ml-[11.5px]'>Accesories</h1>
+                                </div>
+                                <button className='px-24 py-3 border-4 hover:bg-primary hover:text-white border-primary font-Oxanium text-primary mt-[60px] '>Learn more </button>
+                            </div>
+                        </div>
+                        <div className='pt-[67px] bg-white pb-12 px-[61px] font-Raleway  rounded-2xl drop-shadow	'>
+                            <svg width="71" className='mx-auto' height="86" viewBox="0 0 71 86" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.6" d="M0.492676 33.5C0.492676 14.9985 15.4911 0 33.9927 0H36.9631C55.4647 0 70.4631 14.9985 70.4631 33.5C70.4631 52.0015 55.4647 67 36.9631 67H33.9927C15.4911 67 0.492676 52.0015 0.492676 33.5Z" fill="#FFAF0F" />
+                                <path d="M57.5 47.8889H46.5V42.4444L41 37H30L24.5 42.4444V47.8889H13.5C10.475 47.8889 8 50.3389 8 53.3333V66.9444C8 68.9861 9.1 70.7011 10.75 71.6539V80.5556C10.75 83.5772 13.1975 86 16.25 86H54.75C57.8025 86 60.25 83.5772 60.25 80.5556V71.6267C61.8725 70.6739 63 68.9317 63 66.9444V53.3333C63 50.3389 60.525 47.8889 57.5 47.8889ZM30 42.4444H41V47.8889H30V42.4444ZM13.5 53.3333H57.5V66.9444H43.75V58.7778H27.25V66.9444H13.5V53.3333ZM38.25 69.6667H32.75V64.2222H38.25V69.6667ZM54.75 80.5556H16.25V72.3889H27.25V75.1111H43.75V72.3889H54.75V80.5556Z" fill="#F95C19" />
+                            </svg>
+                            <h1 className='font-Raleway font-extrabold text-2xl mt-7 mb-8 text-center mx-auto'>Business Services</h1>
+                            <p className='w-[302px] leading-6 ml-auto font-Raleway mb-[35px] '>We give you complete reliable delivery for your company.  We will take full responsibility of the deliveries.</p>
+                            <div className='text-center'>
+                                <div className='flex items-center font-normal '>
+                                    <svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <ellipse cx="4.2709" cy="3.5" rx="3.65517" ry="3.5" fill="#F95C19" />
+                                    </svg>
+                                    <h1 className='ml-[11.5px]'>Corporate goods</h1>
+                                </div>
+                                <div className='flex items-center  my-[11px]'>
+                                    <svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <ellipse cx="4.2709" cy="3.5" rx="3.65517" ry="3.5" fill="#F95C19" />
+                                    </svg>
+                                    <h1 className='ml-[11.5px]'>Shipment</h1>
+                                </div>
+                                <div className='flex items-center'>
+                                    <svg width="8" height="7" viewBox="0 0 8 7" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                        <ellipse cx="4.2709" cy="3.5" rx="3.65517" ry="3.5" fill="#F95C19" />
+                                    </svg>
+                                    <h1 className='ml-[11.5px]'>Accesories</h1>
+                                </div>
+                                <button className='px-24 py-3 border-4 hover:bg-primary hover:text-white border-primary font-Oxanium text-primary mt-[60px] '>Learn more </button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default Service
